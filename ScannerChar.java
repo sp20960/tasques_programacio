4849913d29ec832e9005e9a7aceab2290ddbf9ea
Sam <sampf2006@gmail.com>
2024-10-03 19:29:59 +0200

    "Afegir tasca calcular darrer caracter de una cadena de caracters"

--- ScannerChar.java
+++ ScannerChar.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
 public class ScannerChar {
     public static void main(String[] args) {
+        String Frase;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Frase: ");
+        Frase = sc.nextLine();
+        Frase = Frase.toUpperCase();
+        System.out.println(Frase);
 
     }
 }
